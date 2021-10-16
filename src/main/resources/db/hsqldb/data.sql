044b51b7bb648fc3cff6d6ef870eac0330242bcc
root <esteban7700@noemail.com>
2021-10-16 17:32:25 +0000

    Taking Paulo's comments into account

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -5,6 +5,8 @@ INSERT INTO vets VALUES (4, 'Rafael', 'Ortega');
 INSERT INTO vets VALUES (5, 'Henry', 'Stevens');
 INSERT INTO vets VALUES (6, 'Sharon', 'Jenkins');
 INSERT INTO types VALUES (7, 'horse');
+INSERT INTO types VALUES (8, 'pony');
+
 
 INSERT INTO specialties VALUES (1, 'radiology');
 INSERT INTO specialties VALUES (2, 'surgery');
